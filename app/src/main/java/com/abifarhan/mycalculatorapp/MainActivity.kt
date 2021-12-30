ed900f5523b9bb794e9bad30f334d79e07a9f42f
abiFarhan <abifarhan04@gmail.com>
2021-12-30 17:09:43 +0700

    Adding Header

--- app/src/main/java/com/abifarhan/mycalculatorapp/MainActivity.kt
+++ app/src/main/java/com/abifarhan/mycalculatorapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.abifarhan.mycalculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,22 +16,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyCalculatorAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Text(text = "Hello Again")
             }
         }
     }
 }
 
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
 
+    MyCalculatorAppTheme {
+        MyApp {
+            content()
+        }
+    }
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyCalculatorAppTheme {
+        MyApp {
+            Text(text = "Hello Again")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader() {
+    androidx.compose.material.Surface() {
+        Column {
+
+        }
     }
 }

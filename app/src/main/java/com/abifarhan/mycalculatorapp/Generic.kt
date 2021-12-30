d8eefa53e93ba3af5664c0d3a6e0b7435b8852d9
abiFarhan <abifarhan04@gmail.com>
2021-12-30 13:30:44 +0700

    Learn Advanced Concept of Kotlin par 2

--- app/src/main/java/com/abifarhan/mycalculatorapp/Generic.kt
+++ app/src/main/java/com/abifarhan/mycalculatorapp/Generic.kt
@@ -1,4 +1,30 @@
 package com.abifarhan.mycalculatorapp
 
-class Generic {
+
+fun main() {
+
+    val listOfItems = listOf("Rafael", "Gina", "George", "James")
+    val intItem = listOf(23,44,55,66)
+    val finder = Finder(list = intItem)
+
+    finder.findItem(23) {
+        if (it == null) {
+            println("Found item null")
+        } else {
+            println("you got it $it")
+        }
+    }
+//    println(listOfItems.sorted())
+//    for (i in 0 until finder)
+}
+
+
+class Finder(private val list: List<Any>) {
+    fun findItem(element: Any, foundItem: (element: Any?) -> Unit) {
+        val itemFoundList = list.filter {
+            it == element
+        }
+
+        if (itemFoundList.isNullOrEmpty()) foundItem(null) else foundItem(itemFoundList[1])
+    }
 }

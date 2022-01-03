e577241bcf9c34323d68f2123f03de9f26d31e43
abiFarhan <abifarhan04@gmail.com>
2022-01-03 18:01:24 +0700

    Adding modifier di

--- app/src/main/java/com/abifarhan/mycalculatorapp/MainActivity.kt
+++ app/src/main/java/com/abifarhan/mycalculatorapp/MainActivity.kt
@@ -3,14 +3,24 @@ package com.abifarhan.mycalculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.abifarhan.mycalculatorapp.ui.theme.MyCalculatorAppTheme
+import com.abifarhan.mycalculatorapp.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,9 +57,24 @@ fun DefaultPreview() {
 @Preview
 @Composable
 fun TopHeader() {
-    androidx.compose.material.Surface() {
-        Column {
-
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(
+                shape =
+                CircleShape.copy(all = CornerSize(12.dp))
+            ),
+        color = Color(0xFFDFB3F2)
+//            .background(color = Color)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Total Per Person")
+            Text(text = "$134")
         }
     }
 }
